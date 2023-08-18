f018c1db68b3077ed4c62c6d180b38a12692a3f5
Gustavo Avila <gustavogavila01@gmail.com>
2023-08-18 17:54:22 -0300

    Início do projeto

---
+++ src/main/java/com/gus/bulktaskreg/BulktaskregApplication.java
@@ -0,0 +1,13 @@
+package com.gus.bulktaskreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulktaskregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulktaskregApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gus/bulktaskreg/BulktaskregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gus.bulktaskreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulktaskregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
